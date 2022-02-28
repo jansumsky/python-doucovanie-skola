21a45748576f703b0a49e2633383faaf00b690a1
sumsky <jan.sumsky@ringieraxelspringer.sk>
2022-02-28 15:48:51 +0100

    ini commit

--- covid_data.py
+++ covid_data.py
@@ -1,7 +1,9 @@
 import csv
 import requests
 import datetime
+
 CSV_URL_BASE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+
 
 def update_numerical_data(existing_list: list, add_list: list) -> list:
     """
@@ -22,7 +24,8 @@ def update_numerical_data(existing_list: list, add_list: list) -> list:
         exit(1)
     return combined_list
 
-def transpose_by_idx(covid_data: list, idx: int)-> dict:
+
+def transpose_by_idx(covid_data: list, idx: int) -> dict:
     """
     Transposes data from list to dict with selectable index
     :param covid_data: raw input as a list
@@ -33,10 +36,11 @@ def transpose_by_idx(covid_data: list, idx: int)-> dict:
     for parsed_entry in covid_data:
         if dict_transposed.get(parsed_entry[idx]):
             tmp_data = dict_transposed.get(parsed_entry[idx])
-            dict_transposed[update_numerical_data(tmp_data, parsed_entry)[idx]] = parsed_entry
+            dict_transposed[parsed_entry[idx]] = update_numerical_data(tmp_data, parsed_entry)
         else:
             dict_transposed.update({parsed_entry[idx]: parsed_entry})
     return dict_transposed
+
 
 def get_covid_data_for_day(day: str, group_by: int) -> dict:
     """
@@ -45,15 +49,15 @@ def get_covid_data_for_day(day: str, group_by: int) -> dict:
     :param group_by: [2: Province, 3: Country, 11: Combined]
     :return: dict of transposed data
     """
-    CSV_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv'
     s = requests.Session()
     download = s.get(CSV_URL_BASE + day + ".csv")
-    reader = csv.reader(download.content.decode('utf-8').strip().split("\n"),delimiter = ",")
+    reader = csv.reader(download.content.decode('utf-8').strip().split("\n"), delimiter=",")
     decoded_content = list(reader)
     transposed_content = transpose_by_idx(decoded_content, group_by)
     return transposed_content
 
-def get_covid_data_day_filtered(day: str, country: str)-> list:
+
+def get_covid_data_day_filtered(day: str, country: str) -> list:
     """
     Return Daily data as a list for a selected country
     :param day: string in format: DD-MM-YYYY
@@ -63,7 +67,8 @@ def get_covid_data_day_filtered(day: str, country: str)-> list:
     data = get_covid_data_for_day(day, 3)
     return data[country]
 
-def get_covid_data_month_filtered(month: str, country: str)-> list:
+
+def get_covid_data_month_filtered(month: str, country: str) -> list:
     """
 
     :param month:
@@ -73,7 +78,8 @@ def get_covid_data_month_filtered(month: str, country: str)-> list:
     monthly_data = []
     return monthly_data
 
-def get_covid_data_year_filtered(year: str, country: str)-> list:
+
+def get_covid_data_year_filtered(year: str, country: str) -> list:
     """
 
     :param year:
@@ -82,6 +88,7 @@ def get_covid_data_year_filtered(year: str, country: str)-> list:
     """
     yearly_data = []
     return yearly_data
+
 
 if __name__ == '__main__':
     print(get_covid_data_day_filtered("01-01-2021", "Slovakia"))
