1c6065217ead1017160d2f640a114799465a5355
sumsky <jan.sumsky@ringieraxelspringer.sk>
2022-03-21 15:01:01 +0100

    add graphing functionality. But not all

--- covid_data.py
+++ covid_data.py
@@ -2,10 +2,52 @@ import csv
 import requests
 import datetime
 
+import matplotlib.pyplot as plt
+import numpy as np
 CSV_URL_BASE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
 
+def get_timestamps(start_date: str = datetime.date.today(), day_count: int=7):
+    timestamps = []
+    if start_date != datetime.date.today():
+        start_date = datetime.datetime.strptime(start_date, '%Y-%M-%d')
 
-def update_numerical_data(existing_list: list, add_list: list) -> list:
+    day_delta = datetime.timedelta(days=1)
+    end_date = start_date + day_count * day_delta
+    for i in range((end_date - start_date).days):
+       timestamps.append(str(start_date + i * day_delta).split(" ")[0])
+    return timestamps
+
+def visualize_data(data, timespan):
+    x = timespan
+    y1, y2, y3, y4 = [], [], [], []
+    plt.rcParams['figure.figsize'] = (10, 13)
+    for entry in data:
+        y1.append(entry[7])
+        y2.append(entry[8])
+        y3.append(entry[9])
+        y4.append(entry[10])
+    plot1 = plt.figure(1)
+
+    plt.subplot(221)
+    plt.plot(x, y1)
+    plt.xticks(rotation=45)
+
+    plt.subplot(222)
+    plt.plot(x, y2)
+    plt.xticks(rotation=45)
+
+    plt.subplot(223)
+    plt.plot(x, y3)
+    plt.xticks(rotation=45)
+
+    plt.subplot(224)
+    plt.plot(x, y4)
+    plt.xticks(rotation=45)
+
+    plt.show()
+
+
+def update_numerical_data(existing_list: list, add_list: list)-> list:
     """
     Updates existing list with defined fields of add_list
     :param existing_list: base list
@@ -25,7 +67,7 @@ def update_numerical_data(existing_list: list, add_list: list) -> list:
     return combined_list
 
 
-def transpose_by_idx(covid_data: list, idx: int) -> dict:
+def transpose_by_idx(covid_data: list, idx: int)-> dict:
     """
     Transposes data from list to dict with selectable index
     :param covid_data: raw input as a list
@@ -51,13 +93,13 @@ def get_covid_data_for_day(day: str, group_by: int) -> dict:
     """
     s = requests.Session()
     download = s.get(CSV_URL_BASE + day + ".csv")
-    reader = csv.reader(download.content.decode('utf-8').strip().split("\n"), delimiter=",")
+    reader = csv.reader(download.content.decode('utf-8').strip().split("\n"),delimiter = ",")
     decoded_content = list(reader)
     transposed_content = transpose_by_idx(decoded_content, group_by)
     return transposed_content
 
 
-def get_covid_data_day_filtered(day: str, country: str) -> list:
+def get_covid_data_day_filtered(day: str, country: str)-> list:
     """
     Return Daily data as a list for a selected country
     :param day: string in format: DD-MM-YYYY
@@ -67,28 +109,25 @@ def get_covid_data_day_filtered(day: str, country: str) -> list:
     data = get_covid_data_for_day(day, 3)
     return data[country]
 
-
-def get_covid_data_month_filtered(month: str, country: str) -> list:
+def get_covid_data_filtered_timespan(country: str, start_date: str, timespan: int)-> list:
     """
 
     :param month:
     :param country:
+    :param start_date:
+    :param timespan:
     :return:
     """
     monthly_data = []
-    return monthly_data
+    for date in get_timestamps(start_date, timespan):
+        tmp_date = datetime.datetime.strptime(date, "%Y-%m-%d" ).strftime("%m-%d-%Y")
+        print(tmp_date)
+        monthly_data.append(get_covid_data_day_filtered(tmp_date, country))
+    visualize_data(monthly_data, get_timestamps(start_date, timespan))
 
 
-def get_covid_data_year_filtered(year: str, country: str) -> list:
-    """
 
-    :param year:
-    :param country:
-    :return:
-    """
-    yearly_data = []
-    return yearly_data
 
 
 if __name__ == '__main__':
-    print(get_covid_data_day_filtered("01-01-2021", "Slovakia"))
+    get_covid_data_filtered_timespan("Slovakia", "2021-01-01", 7)
